1ac655a809b7d776857bab49103be9f4e05c9b6f
Heekwon SHIN <gmlrnjs131@naver.com>
2023-07-20 22:20:09 +0900

    Feat: ForgetPasswordViewController 생성
    Feat: ForgetPasswordViewController 생성

---
+++ WeDoItTogether/WeDoItTogether/Controller/ForgetPasswordViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ForgetPasswordViewController.swift
+//  WeDoItTogether
+//
+//  Created by 신희권 on 2023/07/19.
+//
+
+import UIKit
+
+class ForgetPasswordViewController: UIViewController {
+
+    let forgetPasswordView = ForgetPasswordView()
+    
+    override func loadView() {
+        self.view = forgetPasswordView
+        view.backgroundColor = .white
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      
+    }
+    
+    //로그인버튼 클릭
+
+}
