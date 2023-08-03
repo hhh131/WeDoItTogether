a68663d5bcb585dc583e7e23f90efbf71a0f73e6
Heekwon SHIN <gmlrnjs131@naver.com>
2023-08-03 19:44:27 +0900

    Feat:  비밀번호 변경 이메일 전송기능 추가

--- WeDoItTogether/WeDoItTogether/Controller/Login/ForgetPasswordViewController.swift
+++ WeDoItTogether/WeDoItTogether/Controller/Login/ForgetPasswordViewController.swift
@@ -7,58 +7,56 @@
 
 import UIKit
 import FirebaseAuth
+import Firebase
+import FirebaseDatabase
 
 class ForgetPasswordViewController: UIViewController {
+
     
     let forgetPasswordView = ForgetPasswordView()
-    var userEmail: String?
-    
+    var email: String?
+
     override func loadView() {
-        super.loadView()
         self.view = forgetPasswordView
+
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         setButtons()
     }
-    
-    private func setButtons() {
+
+    func setButtons() {
         forgetPasswordView.findPasswordButton.addTarget(self, action: #selector(touchUpFindPasswordButton), for: .touchUpInside)
     }
-}
 
-//MARK: - 버튼 addTarget
-extension ForgetPasswordViewController {
-    @objc func touchUpFindPasswordButton(_ sender: UIButton) {
-        guard validate() else {
-            return
-        }
-        
-        guard let userEmail = userEmail, userEmail.contains("@"), userEmail.contains(".") else {
-            self.showAlert(message: "이메일 형식으로 채워주세요", yesAction: nil)
-            return
-        }
-        
-        Auth.auth().sendPasswordReset(withEmail: userEmail) { error in
+    @objc func touchUpFindPasswordButton(){
+
+        sendPassword()
+    }
+
+    func sendPassword() {
+        guard let email = forgetPasswordView.emailTextField.text else { return }
+
+        Auth.auth().sendPasswordReset(withEmail: email) { error in
             if let error = error {
-                print(error)
-                self.showAlert(message: "유효하지 않은 이메일 입니다", yesAction: nil)
+                if let ErrorCode = AuthErrorCode.Code(rawValue: (error._code)) {
+                    switch ErrorCode {
+                    case AuthErrorCode.invalidEmail:
+                        self.showAlert(message: "유효하지 않은 이메일 입니다", yesAction: nil)
+                    case AuthErrorCode.missingEmail:
+                        self.showAlert(message: "이메일을 입력 해 주세요", yesAction: nil)
+                    default:
+                        self.showAlert(message: "에러 발생", yesAction: nil)
+                    }
+                }
             }
+            self.showAlert(message: "이메일 발송 완료", yesAction: {
+                ForgetPasswordViewController().dismiss(animated: true)
+            })
+        }
+    }
 
-            self.showAlert(message: "메일을 전송하였습니다. 메일함을 확인해주세요.") {
-                self.navigationController?.popViewController(animated: true)
-            }
-        }
-    }
-    
-    private func validate() -> Bool {
-        guard let userEmail = forgetPasswordView.emailTextField.text, !userEmail.trimmingCharacters(in: .whitespaces).isEmpty else {
-            self.showAlert(message: "이메일을 입력해주세요", yesAction: nil)
-            return false
-        }
-        
-        self.userEmail = userEmail
-        return true
-    }
+
 }
