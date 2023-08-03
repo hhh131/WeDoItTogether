a9ecc8617a31472faa56f834c8b0aaea3c12ea38
HANLeeeee <hanleeeee@naver.com>
2023-08-04 00:52:52 +0900

    Feat: 로그인 후 위치정보 받기

--- WeDoItTogether/WeDoItTogether/Controller/Home/DetailContentViewController.swift
+++ WeDoItTogether/WeDoItTogether/Controller/Home/DetailContentViewController.swift
@@ -7,14 +7,12 @@
 
 import UIKit
 import MapKit
-import CoreLocation
 
-class DetailContentViewController: UIViewController {
+class DetailContentViewController: UIViewController, MKMapViewDelegate {
     
     let detailContentView = DetailContentView()
     var item: Item?
     
-    let locationManager = CLLocationManager()
     let userLocation = UserDefaultsData.shared.getLocation()
     
     override func loadView() {
@@ -27,7 +25,6 @@ class DetailContentViewController: UIViewController {
         
         updateUILabel()
         configureTableView()
-        configureMapView()
     }
     
     func updateUILabel() {
@@ -42,20 +39,6 @@ class DetailContentViewController: UIViewController {
         detailContentView.collectionView.register(MapCollectionViewCell.self, forCellWithReuseIdentifier: MapCollectionViewCell.identifier)
         detailContentView.collectionView.dataSource = self
         detailContentView.collectionView.delegate = self
-    }
-    
-    private func configureMapView() {
-        locationManager.delegate = self
-        self.locationManager.requestWhenInUseAuthorization()
-        
-        DispatchQueue.global().async {
-            if CLLocationManager.locationServicesEnabled() {
-                print("위치 서비스 On 상태")
-                self.locationManager.startUpdatingLocation() //위치 정보 받아오기 시작
-            } else {
-                print("위치 서비스 Off 상태")
-            }
-        }
     }
 }
 
@@ -88,65 +71,5 @@ extension DetailContentViewController: UICollectionViewDelegate, UICollectionVie
         let width: Double = self.view.frame.width
         
         return CGSize(width: width, height: width)
-    }
-}
-
-extension DetailContentViewController: CLLocationManagerDelegate, MKMapViewDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            print("위도: \(location.coordinate.latitude)")
-            print("경도: \(location.coordinate.longitude)")
-            UserDefaultsData.shared.setLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        }
-        
-        locationManager.stopUpdatingLocation()
-    }
-    
-    //사용자의 위치를 못가지고 온 경우
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(#function)
-    }
-    
-    func checkUserDeviceLocationServiceAuthorization(){
-        let authorizationStatus: CLAuthorizationStatus
-        //ios 14.0 이상이라면
-        if #available(iOS 14.0, *){
-            authorizationStatus = locationManager.authorizationStatus
-        } else {
-            authorizationStatus = CLLocationManager.authorizationStatus()
-        }
-        
-        //"iOS 위치서비스 활성화" 여부 체크 : locationServicesEnabled()
-        if CLLocationManager.locationServicesEnabled(){
-            //위치 서비스가 활성화돼 있으므로 위치권한 요청이 가능해서 위치 권한을 요청
-            checkUserCurrentLocationAuthorization(authorizationStatus)
-        } else {
-            print("위치서비스가 꺼져있습니다")
-        }
-    }
-    
-    //사용자의 위치권한 상태 확인
-    func checkUserCurrentLocationAuthorization(_ authorizationStatus: CLAuthorizationStatus) {
-        switch authorizationStatus {
-        case .notDetermined:
-            print("NotDetermined")
-            //정확도 : kCLLocationAccuracy
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            locationManager.requestWhenInUseAuthorization()
-            
-        case .restricted, .denied:
-            print("Denied, 아이폰 설정으로 유도")
-            self.showAlert(message: "위치 서비스를 사용할 수 없습니다. 기기의 '설정>개인정보 보호'에서 위치 서비스를 켜주세요") {
-                if let appSetting = URL(string: UIApplication.openSettingsURLString){
-                    UIApplication.shared.open(appSetting)
-                }
-            }
-        case .authorizedWhenInUse:
-            print("When In Use")
-            locationManager.startUpdatingLocation()
-            
-        default:
-            print("Default")
-        }
     }
 }
