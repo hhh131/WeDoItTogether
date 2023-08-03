80906cb54430087c21fd3b940e17cb32affd6c7c
Heekwon SHIN <gmlrnjs131@naver.com>
2023-08-03 23:30:01 +0900

    Test: pin.title 변경

--- WeDoItTogether/WeDoItTogether/Controller/Home/DetailContentViewController.swift
+++ WeDoItTogether/WeDoItTogether/Controller/Home/DetailContentViewController.swift
@@ -77,7 +77,7 @@ extension DetailContentViewController: UICollectionViewDelegate, UICollectionVie
         
         let pin = MKPointAnnotation()
         pin.coordinate = center
-        pin.title = "나 진짜로 바꿨지롱 ㅋㅋ"
+        pin.title = "최하늘 똥멍청이"
         cell.mapView.addAnnotation(pin)
         
         return cell
