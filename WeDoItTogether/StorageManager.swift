39472da73c943135910dabf3ed0049c67e824c49
bangtori <58802345+bangtori@users.noreply.github.com>
2023-08-04 01:18:31 +0900

    Feat: 파이어베이스 이미지 관리

---
+++ WeDoItTogether/StorageManager.swift
@@ -0,0 +1,64 @@
+//
+//  StorageManager.swift
+//  WeDoItTogether
+//
+//  Created by 방유빈 on 2023/08/03.
+//
+
+import Foundation
+import FirebaseStorage
+import UIKit
+
+final class StorageManager {
+    public enum StorageErrors: Error {
+        case failedToUpload
+        case failedToGetDownloadUrl
+    }
+    
+    static let shared = StorageManager()
+    
+    private let storage = Storage.storage().reference()
+    
+    public typealias UploadPictureCompletion = (Result<String, Error>) -> Void
+    
+    // upload 이미지
+    public func uploadProfilePicture(with data: Data, filePath: String, completion: @escaping UploadPictureCompletion){
+        
+        storage.child("images/\(filePath)").putData(data, metadata: nil) { [weak self] metadata, error in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            guard error == nil else {
+                print("failed to upload data to firebase for picture")
+                completion(.failure(StorageErrors.failedToUpload))
+                return
+            }
+            
+            strongSelf.storage.child("images/\(filePath)").downloadURL { url, error in
+                guard let url = url else {
+                    print("Failed to get download url")
+                    completion(.failure(StorageErrors.failedToGetDownloadUrl))
+                    return
+                }
+                let urlString = url.absoluteString
+                print("download url returned: \(urlString)")
+                completion(.success(urlString))
+            }
+        }
+    }
+    
+    //download 이미지URL
+    public func downloadURL(for path: String, completion: @escaping (Result<URL, Error>) -> Void) {
+        let reference = storage.child(path)
+        reference.downloadURL { url, error in
+            guard let url = url, error == nil else {
+                print(error)
+                print("------------------------------------")
+                completion(.failure(StorageErrors.failedToGetDownloadUrl))
+                return
+            }
+            completion(.success(url))
+        }
+    }
+}
