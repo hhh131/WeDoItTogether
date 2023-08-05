987e3ee8e53407b45df2a18876b823a0e0c9bed6
bangtori <58802345+bangtori@users.noreply.github.com>
2023-08-06 03:34:47 +0900

    Feat : NotificationCenter 설정

---
+++ WeDoItTogether/WeDoItTogether/Extensions/UNUserNotificationCenter+Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  UNUserNotificationCenter+Extensions.swift
+//  WeDoItTogether
+//
+//  Created by 방유빈 on 2023/08/06.
+//
+
+import Foundation
+import UserNotifications
+
+extension UNUserNotificationCenter {
+    func addNotificationRequest(item:Item){
+        //알림 내용 구성
+        let content = UNMutableNotificationContent()
+        content.title = "\(item.title) 약속이 얼마 남지 않았습니다."
+        content.body = "\(item.title)약속까지 한시간 남았습니다. 얼른 \(item.location)으로 이동하세요~"
+        content.sound = .default
+        content.badge = 1
+    
+        //trigger 만들기
+        guard let alarmDate = item.alarmTime else { return }
+        let component = Calendar.current.dateComponents([.year, .day, .hour, .minute], from: alarmDate)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: component, repeats: false)
+        //request만들기
+        let request = UNNotificationRequest(identifier: item.id.uuidString, content: content, trigger: trigger)
+        
+        self.add(request, withCompletionHandler: nil)
+    }
+}
